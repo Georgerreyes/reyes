ec14daa4231fff3ff0a3ef72108b1dbf36e91e97
unknown <georgerreyes4@gmail.com>
2020-05-23 13:12:51 -0400

    prueba

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primera_App_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double temperatura = 35.6;
+
+            int TemperaturaDominicana;
+
+            TemperaturaDominicana =(int) temperatura;
+
+            Console.WriteLine(TemperaturaDominicana);
+
+
+
+
+        }
+    }
+}
