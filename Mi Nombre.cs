f2d0e70be998a48ffbdc526a378aa6fa106eff2d
Georgerreyes <65823638+Georgerreyes@users.noreply.github.com>
2020-05-31 18:33:37 -0400

    Add files via upload

---
+++ Mi Nombre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mi_Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saludos, mi nombre es George Reyes.");
+        }
+    }
+}
