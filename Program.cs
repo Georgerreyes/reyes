a9cb2bbddc260de6d3189c2c9be7788ac687baa9
Georgerreyes <65823638+Georgerreyes@users.noreply.github.com>
2020-05-31 18:19:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aplicacion_de_ITBI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Inserte el Precio del Producto");
+            double precio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ahora por favor digite el ITBIT");
+            double itbit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El precio final es igual a: " +(precio + itbit));
+        }
+    }
+}
