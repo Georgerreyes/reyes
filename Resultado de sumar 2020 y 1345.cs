cb934dae75116a37b71948c8cdcfd311e46b5069
Georgerreyes <65823638+Georgerreyes@users.noreply.github.com>
2020-05-31 18:34:12 -0400

    Add files via upload

---
+++ Resultado de sumar 2020 y 1345.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Resultado_de_sumar_2020_y_1345
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int resultado = (2020 + 1345);
+
+            Console.WriteLine("La suma entre 2020 y 1345 da como resultado " + resultado);
+        }
+    }
+}
