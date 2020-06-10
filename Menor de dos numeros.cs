127b901d9c9e84e1a4c5ff312f2bf6e8baf8e902
Georgerreyes <65823638+Georgerreyes@users.noreply.github.com>
2020-06-09 22:05:37 -0400

    Add files via upload
    09-06

---
+++ Menor de dos numeros.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Menor_de_dos_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte el primer numero");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte el segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 < num2)
+                {
+                    Console.WriteLine("El primer numero es menor: " + num1);
+                }
+            else
+            {
+                if (num2 < num1)
+                {
+                    Console.WriteLine("El segundo numero es menor: " + num2);
+                }
+
+                               
+                
+            }       //George Reyes
+        }
+    }
+}
---
+++ Valor Absoluto de Numero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Valor_Absoluto_de_Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            
+            Console.WriteLine("Ingrese un numero positivo o negativo");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0) 
+            Console.WriteLine("El numero es: " + numero + " y su valor absoluto es: " + Math.Abs(numero));
+            else
+                Console.WriteLine("El numero es " + numero);
+
+
+            //George Reyes 
+            
+
+        }
+    }
+}
